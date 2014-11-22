cf268963d80be114db7fecc0e3e4c319c106dceb
John McLear <john@mclear.co.uk>
2014-11-22 15:09:33 +0000

    sort styling issues

--- static/js/index.js
+++ static/js/index.js
@@ -14,7 +14,8 @@ exports.handleClientMessage_CUSTOM = function(hook, context, cb){
 exports.documentReady = function(){
   var top = $('.toolbar').position().top;
   var bottom = top + $('.toolbar').height();
-  var containerTop = $('.toolbar').position().top + $('.toolbar').height() +5;
+  var containerTop = $('.toolbar').position().top + $('.toolbar').height() - $('#editbar').height();
+  console.log(containerTop);
   $('#editorcontainerbox').css("top", containerTop+"px");
   var popupTop = bottom+4;
   $('#settings, #importexport, #embed, #connectivity, #users').css("top", popupTop+"px");
