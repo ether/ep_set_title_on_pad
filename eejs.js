3b4db42ee8318859f707194a006e4a9ab14eedbc
John McLear <john@mclear.co.uk>
2020-11-26 09:32:21 +0000

    resolve double settings book in hook function

--- eejs.js
+++ eejs.js
@@ -6,4 +6,5 @@ exports.eejsBlock_styles = function (hook_name, args, cb) {
 
 exports.eejsBlock_body = function (hook_name, args, cb) {
   args.content = eejs.require('ep_set_title_on_pad/templates/title.ejs', {settings: false}) + args.content;
+  return cb();
 };
